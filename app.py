b7e94cc74214ce8da1a53f5579082c3b5206a989
Miguel <miguelagustin182@gmail.com>
2024-11-04 14:05:51 -0600

    fix: duplicate entries

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 from datetime import datetime
 import pytz
 import os
 import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
+CORS(app)
+app.debug = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'crowd_data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SECRET!'
+
 db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # habilita websockets
+
 
 local_timezone = pytz.timezone('America/Mexico_City')  # Ajusta según tu zona horaria
 
@@ -24,23 +32,35 @@ class CrowdData(db.Model):
             'timestamp': self.timestamp.isoformat(),
             'cantidad': self.cantidad
         }
+@socketio.on('connect')
+def test_connect():
+    print('client connected')
+      
+@socketio.on('disconnect')
+def test_connect():
+    print('client disonnected')
 
 # Función para manejar mensajes MQTT
 def on_message(client, userdata, msg):
+    print("Mensaje recibido en MQTT:", msg.payload.decode())  # Mensaje de depuración
     cantidad = msg.payload.decode()
     
     with app.app_context():  # Asegurar el contexto de la app
         crowd_data = CrowdData(cantidad=cantidad)
         db.session.add(crowd_data)
         db.session.commit()
-        print(f"Datos almacenados: {cantidad}")
+        socketio.emit('new_data', {'cantidad': cantidad})
+        
+        print(f"Datos almacenados y enviados por websocket: {cantidad}")
+
 
 # Configurar el cliente MQTT
 mqtt_client = mqtt.Client()
 mqtt_client.on_message = on_message
-mqtt_client.connect("localhost", 1883, 60)  # Cambia 'localhost' si el broker está en otro servidor
+mqtt_client.connect("localhost", 1883, 60)
 mqtt_client.subscribe("crowd_data/eventos")
-mqtt_client.loop_start()
+#   mqtt_client.loop_start()
+mqtt_client.loop_forever()
 
 @app.route("/data", methods=["GET"])
 def get_all_data():
@@ -52,4 +72,4 @@ if __name__ == '__main__':
         os.makedirs(os.path.join(basedir, 'instance'))
     with app.app_context():
         db.create_all()  # Crear todas las tablas
-    app.run(debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
